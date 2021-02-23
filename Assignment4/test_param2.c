d9dd125c9ce9b1f5d17c51916021750c20bd45c5
Chen-Yu Chang <cy890509@bu.edu>
2021-02-24 07:18:25 +0800

    add

--- Assignment4/test_param2.c
+++ Assignment4/test_param2.c
@@ -11,11 +11,12 @@
 
 #define NUM_THREADS 10
 
+int a=0;
 /********************/
 void *work(void *i)
 {
     long int j, k;
-    int f = *((int*)(i));  // get the value being pointed to
+    //int f = *((int*)(i));  // get the value being pointed to
     int *g = (int*)(i);    // get the pointer itself
     
     /* busy work */
@@ -24,8 +25,10 @@ void *work(void *i)
         k += j;
     }
     
-    /* printf("Hello World from %lu with value %d\n", pthread_self(), f); */
-    printf("in work(): f=%2d, k=%ld, *g=%d\n", f, k, *g);
+    printf("Hello World from %lu with value %d\n", pthread_self(), g[a]);
+    g[a] = g[a] * 10;
+    a = a + 1;
+    //printf("in work(): f=%2d, k=%ld, *g=%d\n", f, k, *g);
     
     pthread_exit(NULL);
 }
@@ -36,9 +39,10 @@ int main(int argc, char *argv[])
     int i;
     long j, k;
     pthread_t id[NUM_THREADS];
+    int test[NUM_THREADS]={0,1,2,3,4,5,6,7,8,9};
     
     for (i = 0; i < NUM_THREADS; ++i) {
-        if (pthread_create(&id[i], NULL, work, (void *)(&i))) {
+        if (pthread_create(&id[i], NULL, work, (void *)(&test))) {
             printf("ERROR creating the thread\n");
             exit(19);
         }
@@ -48,6 +52,9 @@ int main(int argc, char *argv[])
     k = 0;
     for (j=0; j < 100000000; j++) {
         k += j;
+    }
+    for (k=0;k<NUM_THREADS;k++){
+        printf("Test: %d\n", test[k]);
     }
     
     printf("k=%ld\n", k);
