21e282c981ae47f8d89739511355fa0ef9d72e0c
Chen-Yu Chang <cy890509@bu.edu>
2021-03-03 12:59:03 +0800

    add

--- Assignment5/test_multithread.c
+++ Assignment5/test_multithread.c
@@ -1,163 +1,170 @@
-/*****************************************************************************/
-// gcc -O1 -o part4_final part4_final.c -lm -lrt
+/*************************************************************************/
+// gcc -pthread test_pt.c -lm -lrt -o test_pt
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <pthread.h>
+#include <time.h>
+#include <math.h>
+#include "apple_pthread_barrier.h"
 
 #define GIG 1000000000
-#define CPG 2.4           // Cycles per GHz -- Adjust to your computer
+#define CPG 2.6           // Cycles per GHz -- Adjust to your computer
 
-#define OPTIONS 2
 #define BASE  0
-#define ITERS 1
-#define DELTA 300        // Right now only set up to do one size
+#define ITERS 10
+#define DELTA 100
 
-#define BLOCK_SIZE 4
+#define OPTIONS 2        // Current setting, vary as you wish!
+#define IDENT 0
 
-#define MINVAL   0.0
-#define MAXVAL  100.0
+#define INIT_LOW -10.0
+#define INIT_HIGH 10.0
 
 #define TOL 0.00001
 
-#define O_ITERS 150        // # of OMEGA values to be tested
-#define PER_O_ITERS 10    // trials per OMEGA value
-double OMEGA = 1.90;     // OMEGA base - first OMEGA tested
-#define OMEGA_INC 0.01   // OMEGA increment for each O_ITERS
-int no_rows_per_thread;
-#define NUM_THREADS 4
-long int iterations = 0;
-long int iterations_block = 0;
 typedef double data_t;
 
-/* Create abstract data type for vector -- here a 2D array */
+/* Create abstract data type for matrix */
 typedef struct {
     long int len;
     data_t *data;
-} vec_rec, *vec_ptr;
+} matrix_rec, *matrix_ptr;
 
+int NUM_THREADS = 4;
+
+/* used to pass parameters to worker threads */
 struct thread_data{
-    vec_ptr vec;
-    long int arg;
+    int thread_id;
+    matrix_ptr a;
+    
 };
+double differ;
+pthread_mutex_t diff_lock;
+pthread_barrier_t bar1;
 
-/*****************************************************************************/
-main(int argc, char *argv[])
+/*************************************************************************/
+int main(int argc, char *argv[])
 {
-    int OPTION=0;
+    int OPTION;
     struct timespec diff(struct timespec start, struct timespec end);
     struct timespec time1, time2;
     struct timespec time_stamp[OPTIONS][ITERS+1];
-    int convergence[OPTIONS][ITERS+1];
-    //double convergence[O_ITERS][2];
-    vec_ptr new_vec(long int len);
-    int set_vec_length(vec_ptr v, long int index);
-    long int get_vec_length(vec_ptr v);
-    int init_vector(vec_ptr v, long int len);
-    int init_vector_rand(vec_ptr v, long int len);
-    int print_vector(vec_ptr v);
-    void *SOR(void *arg);
-    void* status;
+    matrix_ptr new_matrix(long int len);
+    int set_matrix_length(matrix_ptr m, long int index);
+    long int get_matrix_length(matrix_ptr m);
+    int init_matrix(matrix_ptr m, long int len);
+    int init_matrix_rand(matrix_ptr m, long int len);
+    int init_matrix_rand_grad(matrix_ptr m, long int len);
+    int zero_matrix(matrix_ptr m, long int len);
+    void pt_cb_base(matrix_ptr a, matrix_ptr b, matrix_ptr c);
+    void pt_cb_pthr(matrix_ptr a, matrix_ptr b, matrix_ptr c);
+    void pt_mb(matrix_ptr a, matrix_ptr b, matrix_ptr c, matrix_ptr d);
+    void pt_ob(matrix_ptr a, matrix_ptr b, matrix_ptr c);
     
-    void *SOR_blocked(void* arg);
     long int i, j, k;
     long int time_sec, time_ns;
-    long int MAXSIZE = (BASE+(ITERS)*DELTA);
-    no_rows_per_thread = MAXSIZE/NUM_THREADS;
+    long int MAXSIZE = BASE+(ITERS)*DELTA;
     
+    printf(" Hello World -- Test SOR pthreads \n");
     
-    pthread_t Threads[NUM_THREADS];
-    struct thread_data data[NUM_THREADS];
+    // declare and initialize the matrix structure
+    matrix_ptr a0 = new_matrix(MAXSIZE);
+    init_matrix_rand(a0, MAXSIZE);
+    matrix_ptr b0 = new_matrix(MAXSIZE);
+    init_matrix_rand(b0, MAXSIZE);
+    matrix_ptr c0 = new_matrix(MAXSIZE);
+    zero_matrix(c0, MAXSIZE);
+    matrix_ptr d0 = new_matrix(MAXSIZE);
+    init_matrix_rand_grad(d0, MAXSIZE);
     
+    /*OPTION = 0;
+     printf("OPTION %d - pt_cb_base()\n", OPTION);
+     for (i = 0; i < ITERS; i++) {
+     init_matrix_rand(a0,BASE+(i+1)*DELTA);
+     set_matrix_length(a0,BASE+(i+1)*DELTA);
+     set_matrix_length(b0,BASE+(i+1)*DELTA);
+     set_matrix_length(c0,BASE+(i+1)*DELTA);
+     clock_gettime(CLOCK_REALTIME, &time1);
+     pt_cb_base(a0,b0,c0);
+     clock_gettime(CLOCK_REALTIME, &time2);
+     time_stamp[OPTION][i] = diff(time1,time2);
+     printf("iter %d done\n", i);
+     }*/
     
-    vec_ptr v0 = new_vec(MAXSIZE);
-    
-    
-    
-    
-    OPTION = 0;
-    for (j = 0; j < ITERS; j++) {
-        // printf("\niter = %d length = %d OMEGA = %0.2f", i, BASE+(i+1)*DELTA, OMEGA);
-        set_vec_length(v0, MAXSIZE);
-        init_vector_rand(v0, MAXSIZE);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-        for(i=0;i<NUM_THREADS; i++)
-        {
-            data[i].vec = v0;
-            data[i].arg = i;
-            pthread_create(&Threads[i], NULL, SOR, (void *)&data[i]);
-        }
-        for(i=0; i<NUM_THREADS; i++)
-        {
-            pthread_join(Threads[i], &status);
-        }
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-        time_stamp[OPTION][j] = diff(time1,time2);
-        convergence[OPTION][j] = iterations;
-        //print_vector(v0);
-        printf("Loop no: %ld\t", j);
-    }
-    
-    
-    OPTION ++;
-    for (j = 0; j < ITERS; j++) {
-        // printf("\niter = %d length = %d OMEGA = %0.2f", i, BASE+(i+1)*DELTA, OMEGA);
-        set_vec_length(v0, MAXSIZE);
-        init_vector_rand(v0, MAXSIZE);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-        for(i=0;i<NUM_THREADS; i++)
-        {
-            data[i].vec = v0;
-            data[i].arg = i;
-            pthread_create(&Threads[i], NULL, SOR_blocked, (void *)&data[i]);
-        }
-        for(i=0; i<NUM_THREADS; i++)
-        {
-            pthread_join(Threads[i], &status);
-        }
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-        time_stamp[OPTION][j] = diff(time1,time2);
-        convergence[OPTION][j] = iterations_block;
-        //print_vector(v0);
-    }
-    
-    
-    //  printf("\n%ld\n", iterations);
-    // printf("\n%ld\n", iterations_block);
+    NUM_THREADS = 16;
+    OPTION =0;
+    printf("OPTION %d: pt_cb_pthr() with %d threads\n", OPTION, NUM_THREADS);
     for (i = 0; i < ITERS; i++) {
-        printf("\n%d, ", (BASE+(i+1)*DELTA));
+        init_matrix_rand(a0,BASE+(i+1)*DELTA);
+        set_matrix_length(a0,BASE+(i+1)*DELTA);
+        set_matrix_length(b0,BASE+(i+1)*DELTA);
+        set_matrix_length(c0,BASE+(i+1)*DELTA);
+        clock_gettime(CLOCK_REALTIME, &time1);
+        pt_cb_pthr(a0,b0,c0);
+        clock_gettime(CLOCK_REALTIME, &time2);
+        time_stamp[OPTION][i] = diff(time1,time2);
+        printf("iter %d done\n", i);
+    }
+    
+    NUM_THREADS = 4;
+    OPTION++;
+    printf("OPTION %d: pt_cb_pthr() with %d threads\n", OPTION, NUM_THREADS);
+    for (i = 0; i < ITERS; i++) {
+        init_matrix_rand(a0,BASE+(i+1)*DELTA);
+        set_matrix_length(a0,BASE+(i+1)*DELTA);
+        set_matrix_length(b0,BASE+(i+1)*DELTA);
+        set_matrix_length(c0,BASE+(i+1)*DELTA);
+        clock_gettime(CLOCK_REALTIME, &time1);
+        pt_cb_pthr(a0,b0,c0);
+        clock_gettime(CLOCK_REALTIME, &time2);
+        time_stamp[OPTION][i] = diff(time1,time2);
+        printf("iter %d done\n", i);
+    }
+    
+    /* enable this to try the experiment on a machine with 8+ cores, and don't
+     forget to also change OPTIONS definition at top! */
+    /*
+     NUM_THREADS = 8;
+     OPTION++;
+     printf("OPTION %d: pt_cb_pthr() with %d threads\n", OPTION, NUM_THREADS);
+     for (i = 0; i < ITERS; i++) {
+     init_matrix_rand(a0,BASE+(i+1)*DELTA);
+     set_matrix_length(a0,BASE+(i+1)*DELTA);
+     set_matrix_length(b0,BASE+(i+1)*DELTA);
+     set_matrix_length(c0,BASE+(i+1)*DELTA);
+     clock_gettime(CLOCK_REALTIME, &time1);
+     pt_cb_pthr(a0,b0,c0);
+     clock_gettime(CLOCK_REALTIME, &time2);
+     time_stamp[OPTION][i] = diff(time1,time2);
+     printf("iter %d done\n", i);
+     }
+     */
+    
+    printf("\n");
+    printf("length, other data:\n");
+    for (i = 0; i < ITERS; i++) {
+        printf("%d, ", BASE+(i+1)*DELTA);
         for (j = 0; j < OPTIONS; j++) {
             if (j != 0) printf(", ");
             printf("%ld", (long int)((double)(CPG)*(double)
-                                     (GIG*time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec))/NUM_THREADS);
-            printf(", %d", convergence[j][i]);
+                                     (GIG * time_stamp[j][i].tv_sec + time_stamp[j][i].tv_nsec)));
         }
+        printf("\n");
     }
-    printf("\n");
+    
+    printf("\n Good Bye World!\n");
     
 }/* end main */
-/*********************************/
 
-struct timespec diff(struct timespec start, struct timespec end)
-{
-    struct timespec temp;
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-    }
-    return temp;
-}
-/* Create 2D vector of specified length per dimension */
-vec_ptr new_vec(long int len)
+/**********************************************/
+/* Create matrix of specified length */
+matrix_ptr new_matrix(long int len)
 {
     long int i;
     
     /* Allocate and declare header structure */
-    vec_ptr result = (vec_ptr) malloc(sizeof(vec_rec));
+    matrix_ptr result = (matrix_ptr) malloc(sizeof(matrix_rec));
     if (!result) return NULL;  /* Couldn't allocate storage */
     result->len = len;
     
@@ -176,54 +183,88 @@ vec_ptr new_vec(long int len)
     return result;
 }
 
-/* Set length of vector */
-int set_vec_length(vec_ptr v, long int index)
+/* Set length of matrix */
+int set_matrix_length(matrix_ptr m, long int index)
 {
-    v->len = index;
+    m->len = index;
     return 1;
 }
 
-/* Return length of vector */
-long int get_vec_length(vec_ptr v)
+/* Return length of matrix */
+long int get_matrix_length(matrix_ptr m)
 {
-    return v->len;
+    return m->len;
 }
 
-/* initialize 2D vector */
-int init_vector(vec_ptr v, long int len)
+/* initialize matrix to consecutive numbers starting with 0 */
+int init_matrix(matrix_ptr m, long int len)
 {
     long int i;
     
     if (len > 0) {
-        v->len = len;
-        for (i = 0; i < len*len; i++) v->data[i] = (data_t)(i);
+        m->len = len;
+        for (i = 0; i < len*len; i++)
+            m->data[i] = (data_t)(i);
         return 1;
     }
     else return 0;
 }
 
-/* initialize vector with random number in a range */
-int init_vector_rand(vec_ptr v, long int len)
+/* initialize matrix to random values in [INIT_LOW, INIT_HIGH] */
+int init_matrix_rand(matrix_ptr m, long int len)
 {
     long int i;
     double fRand(double fMin, double fMax);
     
     if (len > 0) {
-        v->len = len;
+        m->len = len;
         for (i = 0; i < len*len; i++)
-            v->data[i] = (data_t)(fRand((double)(MINVAL),(double)(MAXVAL)));
+            m->data[i] = (data_t)(fRand((double)(INIT_LOW),(double)(INIT_HIGH)));
         return 1;
     }
     else return 0;
 }
 
-/* print vector for test */
-int print_vector(vec_ptr v)
+/* initialize matrix to random values bounded by a "gradient" envelope */
+int init_matrix_rand_grad(matrix_ptr m, long int len)
+{
+    long int i;
+    double fRand(double fMin, double fMax);
+    
+    if (len > 0) {
+        m->len = len;
+        for (i = 0; i < len*len; i++)
+            m->data[i] = (data_t)(fRand((double)(0.0),(double)(i)));
+        return 1;
+    }
+    else return 0;
+}
+
+/* initialize matrix */
+int zero_matrix(matrix_ptr m, long int len)
+{
+    long int i,j;
+    
+    if (len > 0) {
+        m->len = len;
+        for (i = 0; i < len*len; i++)
+            m->data[i] = (data_t)(IDENT);
+        return 1;
+    }
+    else return 0;
+}
+
+data_t *get_matrix_start(matrix_ptr m)
+{
+    return m->data;
+}
+
+/* print matrix */
+int print_matrix(matrix_ptr v)
 {
     long int i, j, len;
     
     len = v->len;
-    printf("\n length = %ld", len);
     for (i = 0; i < len; i++) {
         printf("\n");
         for (j = 0; j < len; j++)
@@ -231,101 +272,108 @@ int print_vector(vec_ptr v)
     }
 }
 
-data_t *get_vec_start(vec_ptr v)
+/*************************************************/
+struct timespec diff(struct timespec start, struct timespec end)
 {
-    return v->data;
+    struct timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
 }
-
-/************************************/
 
 double fRand(double fMin, double fMax)
 {
-    double f = (double)random() / RAND_MAX;
+    double f = (double)random() / (double)(RAND_MAX);
     return fMin + f * (fMax - fMin);
 }
 
-/************************************/
 
-/* SOR */
-void *SOR(void *arg)
+/***************************************************************************/
+/* CPU bound multithreaded code. Here we use pthreads to do the same thing */
+/* as pt_cb_base().  first, the worker thread function                       */
+void *cb_work(void *threadarg)
 {
+    long int i, j, k, low, high;
+    int done = 0;
+    struct thread_data *my_data;
+    my_data = (struct thread_data *) threadarg;
+    int taskid = my_data->thread_id;
+    matrix_ptr a0 = my_data->a;
+    long int length = get_matrix_length(a0);
+    data_t *aM = get_matrix_start(a0);
+    long int n = get_matrix_length(a0);
     
-    //  printf("In the thread\n");
-    long int i, j;
-    struct thread_data *data_thread = (struct thread_data*)arg;
-    vec_ptr v0 = data_thread->vec;
-    long int length = get_vec_length(v0);
-    data_t *data = get_vec_start(v0);
-    double change, mean_change = 100;   // start w/ something big
-    int iters = 0;
+    low = 1 + (taskid * length/NUM_THREADS);//(taskid * length * length)/NUM_THREADS;
+    high = low + length/NUM_THREADS - 1;//((taskid+1)* length * length)/NUM_THREADS;
     
-    long int offset = data_thread->arg;
+    double temp, mydiff = 0;
     
-    long int start = offset*no_rows_per_thread;
-    long int end = start + no_rows_per_thread;
-    
-    while ((mean_change/(double)(length*length)) > (double)TOL) {
-        iters++;
-        mean_change = 0;
-        for (i = start+1; i < end-1; i++)
-            for (j = 1; j < length-1; j++) {
-                change = data[i*length+j] - .25 * (data[(i-1)*length+j] +
-                                                   data[(i+1)*length+j] +
-                                                   data[i*length+j+1] +
-                                                   data[i*length+j-1]);
-                data[i*length+j] -= change * OMEGA;
-                if (change < 0){
-                    change = -change;
-                }
-                mean_change += change;
+    while (!done){
+        mydiff = 0;
+        differ = 0;
+        for (i = low ;i< high; i++){
+            for (j = 1; j<n-1; j++){
+                temp = aM[i*length+j];
+                aM[i*length+j] = 0.2 * (aM[i*length+j] + aM[(i-1)*length+j] + aM[(i+1)*length+j] + aM[i*length+j+1] + aM[i*length+j-1]);
+                mydiff += fabs(aM[i*length+j] - temp);
             }
-        if (abs(data[(length-2)*(length-2)]) > 10.0*(MAXVAL - MINVAL)) {
-            //     printf("\n PROBABLY DIVERGENCE iter = %ld", iters);
-            break;
         }
+        
+        pthread_mutex_lock(&diff_lock);
+        differ+=mydiff;
+        pthread_mutex_unlock(&diff_lock);
+        pthread_barrier_wait(&bar1);
+        
+        if(differ/((n-2)*(n-2))<TOL){
+            done = 1;
+        }
+        
+        pthread_barrier_wait(&bar1);
     }
-    iterations += iters;
+    
+    pthread_exit(NULL);
 }
 
-
-/* SOR w/ blocking */
-void *SOR_blocked(void* arg)
+/* Now, the pthread calling function */
+void pt_cb_pthr(matrix_ptr a, matrix_ptr b, matrix_ptr c)
 {
-    long int i, j, ii, jj;
-    struct thread_data *data_thread = (struct thread_data*)arg;
-    vec_ptr v0 = data_thread->vec;
-    long int length = get_vec_length(v0);
-    data_t *data = get_vec_start(v0);
-    double change, mean_change = 100;
-    int iters = 0;
-    int k;
+    long int i, j, k;
+    pthread_t threads[NUM_THREADS];
+    struct thread_data thread_data_array[NUM_THREADS];
+    int rc;
+    long t;
     
-    long int offset = data_thread->arg;
+    pthread_barrier_init(&bar1, NULL, NUM_THREADS);
+    if (pthread_mutex_init(&diff_lock, NULL) != 0)
+    {
+        printf("\n mutex init has failed\n");
+    }
     
-    long int start = offset*no_rows_per_thread;
-    long int end = start + no_rows_per_thread;
     
-    while ((mean_change/(double)(length*length)) > (double)TOL) {
-        iters++;
-        mean_change = 0;
-        for (ii = start+1; ii < end-1; ii+=BLOCK_SIZE)
-            for (jj = 1; jj < length-1; jj+=BLOCK_SIZE)
-                for (i = ii; i < ii+BLOCK_SIZE; i++)
-                    for (j = jj; j < jj+BLOCK_SIZE; j++) {
-                        change = data[i*length+j] - .25 * (data[(i-1)*length+j] +
-                                                           data[(i+1)*length+j] +
-                                                           data[i*length+j+1] +
-                                                           data[i*length+j-1]);
-                        data[i*length+j] -= change * OMEGA;
-                        if (change < 0){
-                            change = -change;
-                        }
-                        mean_change += change;
-                    }
-        if (abs(data[(length-2)*(length-2)]) > 10.0*(MAXVAL - MINVAL)) {
-            printf("\n PROBABLY DIVERGENCE iter = %d", iters);
-            break;
+    for (t = 0; t < NUM_THREADS; t++) {
+        thread_data_array[t].thread_id = t;
+        thread_data_array[t].a = a;
+        rc = pthread_create(&threads[t], NULL, cb_work,
+                            (void*) &thread_data_array[t]);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
         }
     }
-    iterations_block += iters;
+    
+    for (t = 0; t < NUM_THREADS; t++) {
+        if (pthread_join(threads[t],NULL)){
+            printf("ERROR; code on return from join is %d\n", rc);
+            exit(-1);
+        }
+    }
+    
+    pthread_barrier_destroy(&bar1);
+    pthread_mutex_destroy(&diff_lock);
+    
 }
