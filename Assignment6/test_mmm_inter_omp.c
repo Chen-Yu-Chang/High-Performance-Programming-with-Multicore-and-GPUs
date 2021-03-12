3cd43e68919def0f322e6507f21d64974b6ecf09
Chen-Yu Chang <cy890509@bu.edu>
2021-03-12 14:25:36 +0800

    add

--- Assignment6/test_mmm_inter_omp.c
+++ Assignment6/test_mmm_inter_omp.c
@@ -323,12 +323,12 @@ void mmm_ijk_omp(matrix_ptr a, matrix_ptr b, matrix_ptr c)
     
 #pragma omp parallel shared(a0,b0,c0,row_length) private(i,j,k,sum)
     {
-
+        //#pragma omp for
         for (i = 0; i < row_length; i++) {
-            
+            #pragma omp for
             for (j = 0; j < row_length; j++) {
                 sum = 0;
-                #pragma omp for
+                //#pragma omp for
                 for (k = 0; k < row_length; k++)
                     sum += a0[i*row_length+k] * b0[k*row_length+j];
                 c0[i*row_length+j] += sum;
@@ -372,12 +372,12 @@ void mmm_kij_omp(matrix_ptr a, matrix_ptr b, matrix_ptr c)
     
 #pragma omp parallel shared(a0,b0,c0,row_length) private(i,j,k,r)
     {
-
+        //#pragma omp for
         for (k = 0; k < row_length; k++) {
-            
+            #pragma omp for
             for (i = 0; i < row_length; i++) {
                 r = a0[i*row_length+k];
-                #pragma omp for
+                //#pragma omp for
                 for (j = 0; j < row_length; j++)
                     c0[i*row_length+j] += r*b0[k*row_length+j];
             }
